ff2ec98fd19af81f1a22aa935621fd22a5630a6e
Daniel Ayala <dannyboy3385@gmail.com>
2020-09-28 15:19:05 -0700

    brought in React Table and added functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import 'react-table-v6/react-table.css'
+import axios from 'axios'
+import ReactTable from 'react-table-v6'
 
-function App() {
+
+const App = () => {
+
+  const [employeeState, setEmployeeState] = useState({
+    employees: [],
+    columns: [
+      {
+        Header: 'Name',
+        accessor: 'name'
+      },
+      {
+        Header: 'Phone',
+        accessor: 'phone'
+      },
+      {
+        Header: 'Email',
+        accessor: 'email'
+      },
+      {
+        Header: 'Age',
+        accessor: 'age'
+      },
+      {
+        Header: 'Gender',
+        accessor: 'gender'
+      },
+    ]
+  })
+
+
+  useEffect(() => {
+    axios.get('https://randomuser.me/api?results=20')
+      .then(({ data }) => {        
+        let employees = data.results.map(employee => ({
+          name: `${employee.name.first} ${employee.name.last}`,
+          email: employee.email,
+          phone: employee.phone,
+          age: employee.dob.age,
+          gender: employee.gender
+        }))
+
+        setEmployeeState({ ...employeeState, employees })
+      })
+      .catch(err => console.error(err))
+  }, [])
+
   return (
-    <>
-    <h1>Hello world!</h1>
-    <h4>How are you!</h4>
-    <h6>I Hate You!</h6>
-    </>
-  );
+    <ReactTable
+      data={employeeState.employees}
+      columns={employeeState.columns}
+    />
+  )
 }
 
-export default App;
+export default App
