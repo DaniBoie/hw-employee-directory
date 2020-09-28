01004721d63ef4196425527eea7ad63b6723c074
Daniel Ayala <dannyboy3385@gmail.com>
2020-09-28 15:36:39 -0700

    Updates

---
+++ precache-manifest.8d4bed3f6358e838c7150276a4b38fc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ca22c03bc3cb01081b8a3f1744ed75",
+    "url": "/hw-employee-directory/index.html"
+  },
+  {
+    "revision": "ff129896df8940ef95be",
+    "url": "/hw-employee-directory/static/css/2.d17711cb.chunk.css"
+  },
+  {
+    "revision": "8d9e53e0c8e1c0e130ae",
+    "url": "/hw-employee-directory/static/css/main.c1d51de4.chunk.css"
+  },
+  {
+    "revision": "ff129896df8940ef95be",
+    "url": "/hw-employee-directory/static/js/2.6db93337.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hw-employee-directory/static/js/2.6db93337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d9e53e0c8e1c0e130ae",
+    "url": "/hw-employee-directory/static/js/main.f922d13d.chunk.js"
+  },
+  {
+    "revision": "b09a723639739ee6e7e7",
+    "url": "/hw-employee-directory/static/js/runtime-main.651da033.js"
+  }
+]);
